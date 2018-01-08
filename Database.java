48d5654ef89e70d808c3ab63fcc83eb3a7e57749
th-ht-zaw <tzaw@stuy.edu>
2018-01-08 17:30:54 -0500

    devlog update and Database

---
+++ Database.java
@@ -0,0 +1,18 @@
+public class Database {
+
+    
+    //The main print method
+    public String printinformation(String tea) {
+	String information = ""
+	if (tea == "") {
+	    information = "";
+	    System.out.println(information);
+	    return information;
+	}
+	//If statements should be following this format
+	//Will update what is within the Strings once research is done
+	//Question: Do we want to make the method "void" and make it not return anything? 
+    } 
+
+
+} 
