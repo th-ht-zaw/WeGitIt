5253144b13d3f17d18ffe1735df643e3aead5b43
ennylgnaw <lwang7@stuy.edu>
2018-01-08 22:57:17 -0500

    Modified Woo.java so it can compile and run

--- Code/Woo.java
+++ Code/Woo.java
@@ -1,24 +1,34 @@
 import cs1.Keyboard;
 
 //Driver
-public class Woo implements Comparable {
+public class Woo {//implements Comparable {
 
     //instance vars
-    private static final String[] _tealist = new String[]; //may not be final if user adds tea, list of teas
-    private String[] _teachoices = new String[]; //
+    private static final String[] _tealist = new String[10]; //may not be final if user adds tea, list of teas
+    private String[] _teachoices; //
 
+    //Default constructor
+    public Woo() {
+	_teachoices = new String[10];
+    }
+    
     //Methods
     public void Narrow (String SympOrFlav) {
 	//implementation	
     }
 
-    public void populate {
+    public void populate() {
+	//implementation
+    }
+
+    public int compareTo( String a ) {
+	return 0;
 	//implementation
     }
 
     //main
-    public static void main (String[][] args) {
-	//implementation
+    public static void main (String[] args) {
+        System.out.println("Compiles and runs!");
     }
 
 }//end
